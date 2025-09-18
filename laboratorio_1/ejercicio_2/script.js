9954f79ad46cd8d4255c7f5ab583695b168f8fcc
Jolman G. <43827425+dimitryx01@users.noreply.github.com>
2025-09-18 20:00:46 +0200

    ejercicio 2 completado

---
+++ laboratorio_1/ejercicio_2/script.js
@@ -0,0 +1,28 @@
+
+function variables(){
+
+    var num1 = 2;
+    var num2 = 4;
+    var caracteres = "esto es una cadena de caracteres";
+    var booleano = true;
+    var resultado ;
+
+    resultado = num1+num2;
+    console.log("la suma de "+num1+" y "+num2+" el resultado es: "+resultado)
+
+    resultado = num1-num2;
+    console.log("la resta de "+num1+" y "+num2+" el resultado es: "+resultado)
+
+   resultado = num1*num2;
+    console.log("la multiplicacion de "+num1+" y "+num2+" el resultado es: "+resultado)
+
+    resultado = num1/num2;
+    console.log("la division de "+num1+" y "+num2+" el resultado es: "+resultado)
+
+    resultado = num1%num2;
+    console.log("el modulo de "+num1+" y "+num2+" el resultado es: "+resultado)
+
+    resultado=resultado+caracteres;
+    console.log("Reto cumplido: "+resultado);
+}
+
