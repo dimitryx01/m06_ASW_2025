92177e22d72131edf8f09048f6ce9d4c49edafc1
Jolman G. <43827425+dimitryx01@users.noreply.github.com>
2025-09-18 20:23:25 +0200

    falta el while

---
+++ laboratorio_1/ejercicio_3/script.js
@@ -0,0 +1,23 @@
+
+function edad() {
+    var edad = prompt("Ingrese su edad");
+
+    if (edad >= 0 && edad < 100) {
+
+        if (edad < 18) {
+            console.log("es menor de edad");
+        } else {
+            console.log("es mayor de edad");
+        }
+    }
+    else {
+        console.log("edad no valida");
+    }
+
+for(i=0;i<=10;i++){
+    console.log("Variable i del bucle for: "+i);
+
+}
+
+}
+
