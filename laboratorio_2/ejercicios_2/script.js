31686d795f0bfd1a63d1d892532329660a489aac
Jolman G. <43827425+dimitryx01@users.noreply.github.com>
2025-09-30 15:43:56 +0200

    ejercicio 2 terminado

---
+++ laboratorio_2/ejercicios_2/script.js
@@ -0,0 +1,28 @@
+let dato;
+
+function juego() {
+    var numero = Math.floor(Math.random() * 100) + 1;
+    
+    for (let i = 1; i <= 7; i++) {
+        dato = prompt('Intento ' + i + '/7. Adivina el numero (1-100):');
+        dato = parseInt(dato);
+
+        if (dato > numero) {
+            console.log('El numero ingresado es mayor');
+            window.alert('El numero ingresado es mayor');//lo puse con windows por que con console log no sirve
+        } else if (dato < numero) {
+            console.log('El numero ingresado es menor');
+            window.alert('El numero ingresado es menor');
+        } else {
+            console.log('¡GANASTE!');
+            console.log('Tus intentos fueron: ' + i);
+            window.alert('¡GANASTE!');
+            window.alert('Tus intentos fueron: ' + i);
+            return; // Salir de la función si gana
+        }
+        
+        if (i === 7) {
+            console.log('Has perdido. El número era ' + numero);
+        }
+    }
+}
---
+++ laboratorio_2/ejercicios_5/script.js
@@ -0,0 +1,7 @@
+class alumno{
+    constructor(nombre,edad,nota){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.nota = nota;
+    }
+}
