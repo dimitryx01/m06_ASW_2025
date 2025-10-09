b1fc7773febd1a6bf0945cffcaeb9cccb1de931e
Jolman G. <43827425+dimitryx01@users.noreply.github.com>
2025-10-09 20:54:48 +0200

    empezando el laboratorio 3

---
+++ laboratorio_3/script.js
@@ -0,0 +1,36 @@
+
+const ob_nombre = document.getElementById('nombre');
+
+
+function lanzar(){
+
+
+};
+
+
+lanzar();
+
+function guardar_preferencias(){
+
+console.log("funcion llamada")
+
+}
+
+
+
+
+var boton = document.getElementById('save')
+
+const bgcolor = document.getElementById('bgColor')
+const name = document.getElementById('nickname');
+
+boton.addEventListener('click',()=>{
+    const color = bgcolor.value;
+    const name2 = name.value;
+
+    document.body.style.backgroundColor = color;
+    ob_nombre.textContent = name2;
+    
+}
+)
+
